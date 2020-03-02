9947779278f63ddc954fe96021c7838e5c36762c
Marc Jordan <marc@plutovr.com>
2020-03-02 11:06:49 -0800

    use a different env variable

--- tools/steamworks_sdk_dir.js
+++ tools/steamworks_sdk_dir.js
@@ -5,6 +5,8 @@ const path = require('path')
 // Allow setting a different steamworks_sdk path via an environment variable.
 if (process.env.STEAMWORKS_SDK_PATH) {
   console.log(process.env.STEAMWORKS_SDK_PATH)
+} else if (process.env.npm_config_STEAMWORKS_SDK_PATH) {
+	console.log(process.env.npm_config_STEAMWORKS_SDK_PATH)
 } else {
   // Otherwise, use the default path (deps/steamworks_sdk)
   console.log(path.join(__dirname, '..', 'deps', 'steamworks_sdk'))
